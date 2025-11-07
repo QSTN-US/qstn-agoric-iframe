00312c707de9be4a8fdc46b568437d34cb997422
JoE11-y <Josephedoh77@gmail.com>
2025-11-07 11:58:31 +0100

    Inline JavaScript into HTML for iframe compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+// Custom plugin to inline JavaScript into HTML
+class InlineScriptPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('InlineScriptPlugin', (compilation) => {
+      HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync(
+        'InlineScriptPlugin',
+        (data, callback) => {
+          const { html, plugin } = data;
+
+          // Find script tags
+          const scriptRegex = /<script\s+src="([^"]+)"[^>]*><\/script>/g;
+          let newHtml = html;
+          let match;
+
+          while ((match = scriptRegex.exec(html)) !== null) {
+            const scriptSrc = match[1];
+            const scriptFilename = scriptSrc.replace('./', '');
+
+            // Get the JS content from compilation
+            const jsAsset = compilation.assets[scriptFilename];
+            if (jsAsset) {
+              const jsContent = jsAsset.source();
+              // Replace script tag with inline script
+              newHtml = newHtml.replace(
+                match[0],
+                `<script>${jsContent}</script>`
+              );
+              console.log(`[InlineScriptPlugin] Inlined ${scriptFilename} (${(jsContent.length / 1024).toFixed(0)}KB)`);
+
+              // Delete the separate JS file since it's now inlined
+              delete compilation.assets[scriptFilename];
+              delete compilation.assets[scriptFilename + '.LICENSE.txt'];
+            }
+          }
+
+          data.html = newHtml;
+          callback(null, data);
+        }
+      );
+    });
+  }
+}
 
 module.exports = {
   entry: './src/index.js',
@@ -28,13 +72,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/template.html',
       filename: 'agoric-sandbox.html',
-      inject: 'body', // Inject script tag at end of body
-      scriptLoading: 'blocking', // Use blocking script loading for reliability
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-      },
+      inject: 'body',
+      scriptLoading: 'blocking',
+      minify: false, // Disable minify for debugging
     }),
+    new InlineScriptPlugin(), // Inline the JavaScript after HTML is generated
   ],
   resolve: {
     fallback: {
